14954ab23ebd4894f1fbeaf48faaad95907f2a80
Tom McKeesick <tmck01@gmail.com>
2019-10-24 10:08:09 +1100

    Return response headers with data and status

--- lib/oktakit/client.rb
+++ lib/oktakit/client.rb
@@ -172,7 +172,7 @@ module Oktakit
       uri = URI::DEFAULT_PARSER.escape("/api/v1" + path.to_s)
       @last_response = resp = sawyer_agent.call(method, uri, data, options)
 
-      response = [resp.data, resp.status]
+      response = [resp.data, resp.status, resp.headers]
       response << absolute_to_relative_url(resp.rels[:next]) if paginate
       response
     end
